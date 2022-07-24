5b8e87420e9491932309767a8db6746359fa6406
drewzag <megafurfur@gmail.com>
2022-07-24 13:32:24 +0300

    fix: change number position

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -200,20 +200,20 @@ const App = () => {
         DEL
       </button>
       <OperationButton operation='/' dispatch={dispatch} />
-      <DigitButton digit='1' dispatch={dispatch} />
-      <DigitButton digit='2' dispatch={dispatch} />
-      <DigitButton digit='3' dispatch={dispatch} />
+      <DigitButton digit='7' dispatch={dispatch} />
+      <DigitButton digit='8' dispatch={dispatch} />
+      <DigitButton digit='9' dispatch={dispatch} />
       <OperationButton operation='*' dispatch={dispatch} />
       <DigitButton digit='4' dispatch={dispatch} />
       <DigitButton digit='5' dispatch={dispatch} />
       <DigitButton digit='6' dispatch={dispatch} />
       <OperationButton operation='+' dispatch={dispatch} />
-      <DigitButton digit='7' dispatch={dispatch} />
-      <DigitButton digit='8' dispatch={dispatch} />
-      <DigitButton digit='9' dispatch={dispatch} />
+      <DigitButton digit='1' dispatch={dispatch} />
+      <DigitButton digit='2' dispatch={dispatch} />
+      <DigitButton digit='3' dispatch={dispatch} />
       <OperationButton operation='-' dispatch={dispatch} />
-      <DigitButton digit='.' dispatch={dispatch} />
       <DigitButton digit='0' dispatch={dispatch} />
+      <DigitButton digit='.' dispatch={dispatch} />
       <button
         className='span-two'
         onClick={() => {
